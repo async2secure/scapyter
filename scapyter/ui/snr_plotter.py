4997cbe6651487a03f0a8f5952ed0a8aa7016307
Nay Aung Kyaw <aknay@outlook.com>
2026-07-20 15:57:27 +0800

    add multiple byte snr service

--- scapyter/ui/snr_plotter.py
+++ scapyter/ui/snr_plotter.py
@@ -3,15 +3,33 @@ import numpy as np
 
 
 class SnrPlotter:
-    def __init__(self, data: np.ndarray):
+    def __init__(self, data: dict[int, np.ndarray]):
         self._data = data
 
-    def plot(self):
-        plt.figure(figsize=(10, 4))
-        plt.plot(self._data, linewidth=1, color="red")
+    def plot_byte(self, byte_location: int):
+        snr = self._data[byte_location]
+
+        peak = np.argmax(snr)
+
+        plt.figure(figsize=(12, 4))
+
+        plt.plot(
+            snr,
+            color="red",
+        )
+
+        plt.scatter(
+            peak,
+            snr[peak],
+            color="blue",
+        )
+
+        plt.title(
+            f"Byte {byte_location} | " f"Peak sample={peak}, " f"SNR={snr[peak]:.3f}"
+        )
 
         plt.xlabel("Samples")
         plt.ylabel("SNR")
+        plt.grid(alpha=0.3)
 
-        plt.tight_layout()
         plt.show()
