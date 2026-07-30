3a9c25a50bfe8ce9c4fc234595b74dedacfa72ce
aknay <aknay@outlook.com>
2026-07-30 14:52:00 +0800

    add progressive CPA

---
+++ scapyter/domain/analysis/correlation/value_objects/progressive_cpa_result.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+from scapyter.domain.value_object import CpaByteResult
+
+
+@dataclass(frozen=True)
+class ProgressiveCpaResult:
+    processed_traces: int
+    byte_results: list[CpaByteResult]
